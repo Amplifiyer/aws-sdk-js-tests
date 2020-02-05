eb4243750a121de8c6618953069e4c2aae40973e
pravgupt@amazon.com <pravgupt@8c85905cd208.ant.amazon.com>
2020-02-04 20:15:21 -0800

    lexruntime client repro

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const AWS = require("aws-sdk");
-const Rekognition = require("aws-sdk/clients/rekognition");
+const lexruntime = require("aws-sdk/clients/lexruntime");
 
 const {
-  RekognitionClient,
-  DetectTextCommand
-} = require("@aws-sdk/client-rekognition");
+  LexRuntimeServiceClient,
+  PostTextCommand
+} = require("@aws-sdk/client-lex-runtime-service");
 const {
   fromCognitoIdentityPool
 } = require("@aws-sdk/credential-provider-cognito-identity");
@@ -28,19 +28,26 @@ const getHTMLElement = (title, content) => {
   return element;
 };
 
-const componentV2 = async file => {
+const componentV2 = async () => {
   // Initialize the Amazon Cognito credentials provider
   AWS.config.region = REGION;
   const creds = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: IDENTITY_POOL_ID
   });
   AWS.config.credentials = creds;
-  const v2Client = new Rekognition({ REGION, creds });
+  const v2Client = new lexruntime({ REGION, creds });
   let response = null,
     error = null;
 
   try {
-    response = await v2Client.detectText({ Image: { Bytes: file } }).promise();
+    response = await v2Client
+      .postText({
+        botAlias: "$LATEST",
+        botName: "BookTrip_devoa",
+        inputText: "Book My Hotel",
+        userId: ""
+      })
+      .promise();
   } catch (e) {
     error = e;
   }
@@ -51,8 +58,8 @@ const componentV2 = async file => {
   );
 };
 
-const componentV3 = async file => {
-  const v3Client = new RekognitionClient({
+const componentV3 = async () => {
+  const v3Client = new LexRuntimeServiceClient({
     region: REGION,
     credentials: fromCognitoIdentityPool({
       client: new CognitoIdentityClient({
@@ -67,7 +74,12 @@ const componentV3 = async file => {
 
   try {
     response = await v3Client.send(
-      new DetectTextCommand({ Image: { Bytes: file } })
+      new PostTextCommand({
+        botAlias: "$LATEST",
+        botName: "BookTrip_devoa",
+        inputText: "Book My Hotel",
+        userId: ""
+      })
     );
   } catch (e) {
     error = e;
@@ -79,41 +91,12 @@ const componentV3 = async file => {
   );
 };
 
-async function identifyFromFile(event) {
-  console.log("Identifying...");
-  const {
-    target: { files }
-  } = event;
-  const [file] = files || [];
-
-  if (!file) {
-    return;
-  }
+(async () => {
   try {
-    document.body.appendChild(await componentV2(await blobToArrayBuffer(file)));
-    document.body.appendChild(await componentV3(await blobToArrayBuffer(file)));
+    document.body.appendChild(await componentV2());
+    document.body.appendChild(await componentV3());
   } catch {
     // Regardless execute V3
-    document.body.appendChild(await componentV3(await blobToArrayBuffer(file)));
+    document.body.appendChild(await componentV3());
   }
-}
-window.onchange = identifyFromFile;
-
-function blobToArrayBuffer(blob) {
-  return new Promise((res, rej) => {
-    const reader = new FileReader();
-    reader.onload = _event => {
-      res(reader.result);
-    };
-    reader.onerror = err => {
-      rej(err);
-    };
-    try {
-      reader.readAsArrayBuffer(blob);
-    } catch (err) {
-      rej(err); // in case user gives invalid type
-    }
-  });
-}
-
-(async () => {})();
+})();
